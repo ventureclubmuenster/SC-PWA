c11dc6bba626f2262f779e2fd182eb037123c7fc
Yannick Brusa <yannickbrusa@MacBook-Air-von-Yannick.local>
2026-03-25 14:37:56 +0100

    fix haptic

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,5 @@
 import BottomBar from '@/components/BottomBar';
 import DemoBanner from '@/components/DemoBanner';
-import HapticProvider from '@/components/HapticProvider';
 import { DataProvider } from '@/components/DataProvider';
 import AddToHomeScreenPrompt from '@/components/AddToHomeScreenPrompt';
 
@@ -11,16 +10,14 @@ export default function DashboardLayout({
 }) {
   return (
     <DataProvider>
-      <HapticProvider>
-        <div className="min-h-screen pb-20">
-          <DemoBanner />
-          <AddToHomeScreenPrompt />
-          <main className="mx-auto max-w-lg px-4 pt-6">
-            {children}
-          </main>
-          <BottomBar />
-        </div>
-      </HapticProvider>
+      <div className="min-h-screen pb-20">
+        <DemoBanner />
+        <AddToHomeScreenPrompt />
+        <main className="mx-auto max-w-lg px-4 pt-6">
+          {children}
+        </main>
+        <BottomBar />
+      </div>
     </DataProvider>
   );
 }
